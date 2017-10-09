22f3ea44fdf56f6eea0caaebc3259c59624e81b2
Josh Nothum <joshnothum@gmail.com>
2017-10-09 06:35:30 -0500

    inputs now clear

--- client.js
+++ client.js
@@ -8,10 +8,41 @@ function readyNow(){
 }
 function clickHandler() {
   $("#submitNow").on('click', recordNow);
+  $('#submitNow').on('click', clearText);
 
 
 }
-
+var employeeArray ={};
 function recordNow(){
-  $
+  var firstName = $('#nameField').val();
+  var lastName = $('#lastNameField').val();
+  var annualSalary = $('#salaryField').val();
+  var employeeID = $('#employeeIDField').val();
+  var jobTitle  =$('#jobTitleField').val();
+  var monthlyCost = Math.round(($('#salaryField').val()/12));
+
+  $(".answers").append("<p>" + firstName + "</p>");
+  console.log(firstName);
+
+  $(".answers").append("<p>" + lastName + "</p>");
+  console.log(lastName);
+
+  $(".answers").append("<p>" + employeeID + "</p>");
+  console.log(employeeID);
+  $('answers').append("<p>" + jobTitle + "</p>");
+
+  $(".answers").append("<p>" + annualSalary + "</p>");
+  console.log(annualSalary);
+
+  $(".answers").append('<p>' + 'Monthly cost:', monthlyCost + '</p>')
+  console.log(monthlyCost);
+}
+
+function clearText() {
+  $('#nameField').val('');
+  $('#lastNameField').val('');
+  $('#salaryField').val('');
+  $('#employeeIDField').val('');
+  $('#jobTitleField').val('');
+
 }
