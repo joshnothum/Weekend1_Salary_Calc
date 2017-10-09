bcda68db61a22125732ad482aa676daafa55cc6e
Josh Nothum <joshnothum@gmail.com>
2017-10-09 09:08:34 -0500

    tried storage array

--- client.js
+++ client.js
@@ -9,17 +9,20 @@ function readyNow(){
 function clickHandler() {
   $("#submitNow").on('click', recordNow);
   $('#submitNow').on('click', clearText);
-
+  $('#submitNow').on('click', addItUp);
 
 }
-var employeeArray ={};
+
+var employeeArray =[];
+var monthlyCost;
+
 function recordNow(){
   var firstName = $('#nameField').val();
   var lastName = $('#lastNameField').val();
   var annualSalary = $('#salaryField').val();
   var employeeID = $('#employeeIDField').val();
   var jobTitle  =$('#jobTitleField').val();
-  var monthlyCost = Math.round(($('#salaryField').val()/12));
+  var monthlyCost= (annualSalary)/12;
 
   $(".answers").append("<p>" + firstName + "</p>");
   console.log(firstName);
@@ -32,10 +35,15 @@ function recordNow(){
   $('answers').append("<p>" + jobTitle + "</p>");
 
   $(".answers").append("<p>" + annualSalary + "</p>");
+  employeeArray.push(monthlyCost);
   console.log(annualSalary);
 
   $(".answers").append('<p>' + 'Monthly cost:', monthlyCost + '</p>')
   console.log(monthlyCost);
+
+  return employeeArray
+  console.log(employeeArray);
+
 }
 
 function clearText() {
@@ -46,3 +54,13 @@ function clearText() {
   $('#jobTitleField').val('');
 
 }
+
+function addItUp(employeeArray) {
+  for(i=0; i< employeeArray.length; i++ )
+  monthlyCost=monthlyCost+monthlyCost;
+  employeeArray.push(monthlyCost);
+  return monthlyCost
+
+}
+console.log(monthlyCost);
+console.log(employeeArray);
