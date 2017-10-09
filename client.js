ce5f1fdb8ec60664c468b6c117d5a258e2af7ffe
Josh Nothum <joshnothum@gmail.com>
2017-10-09 06:34:34 -0500

    button is functional

---
+++ client.js
@@ -0,0 +1,17 @@
+console.log('js sourced.');
+
+$(document).ready(readyNow);
+
+function readyNow(){
+  clickHandler();
+
+}
+function clickHandler() {
+  $("#submitNow").on('click', recordNow);
+
+
+}
+
+function recordNow(){
+  $
+}
